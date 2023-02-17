8cb92267f77cb77f471e12fc6a3bf1012d1398ce
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-02-17 15:37:44 +0800

    Create Mouse.java

---
+++ Inheritance/Mouse.java
@@ -0,0 +1,19 @@
+
+public class Mouse {
+
+	public static void leftClick() {
+		System.out.println("Clicked!");
+	}
+	
+	public static void rightClick() {
+		System.out.println("Clicked!");
+	}
+	
+	public static void scrollUp() {
+		System.out.println("Scroll Up!");
+	}
+	
+	public static void scrollDown() {
+		System.out.println("Scroll Down!");
+	}
+}
