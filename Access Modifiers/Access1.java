bf0fda2d8bebcc6ddfa42486df04df82665c9033
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-02-18 16:16:32 +0800

    Create Access1.java

---
+++ Access Modifiers/Access1.java
@@ -0,0 +1,23 @@
+package package1;
+
+public class Access1 {
+	
+	//default, public, private, and protected.
+	
+	//default modifiers
+//	int hours = 3;
+//	int minutes = 47;
+	
+	//public modifiers
+//	public int hours = 3;
+//	public int minutes = 47;
+	
+	//private like it said this modifiers keep it private ;>
+//	private int hours = 3;
+//	private int minutes = 47;
+	
+	//protected
+	protected int hours = 3;
+	protected int minutes = 47;
+
+}
