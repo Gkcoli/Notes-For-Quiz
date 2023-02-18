ae470c9b26396e1bd6ab6eec8c0b458fcb5feb7f
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-02-18 16:17:19 +0800

    Create Access3.java

---
+++ Access Modifiers/Access3.java
@@ -0,0 +1,17 @@
+package package2;
+
+import package1.Access1;
+
+public class Access3 {
+	
+	public static void main(String[]args) {
+//		or we can do public class Access3 extends Access1{
+		//}
+		// so this one can inherit what Access1 has
+		
+		// if you remove the  extends to Access1 it will return again to
+		// default.
+		
+	}
+
+}
