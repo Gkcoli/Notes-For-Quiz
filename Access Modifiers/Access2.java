6dc8cdbaaeb55336351587c954a71265b2e2c26c
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-02-18 16:16:45 +0800

    Create Access2.java

---
+++ Access Modifiers/Access2.java
@@ -0,0 +1,26 @@
+package package1;
+
+public class Access2 {
+
+	public static void main(String[] args) {
+// default modifiers 		
+//		Access1 a = new Access1();
+//		System.out.println(a.hours);
+//		System.out.println(a.minutes);
+
+// public modifiers
+//		Access1 a = new Access1(); 
+//		System.out.println(a.hours);
+//		System.out.println(a.minutes);
+		
+//private modifiers keeps the code private
+		
+//protected
+		//only this have a access it doe'snt go at the other package
+		Access1 a = new Access1(); 
+		System.out.println(a.hours);
+		System.out.println(a.minutes);
+		
+	}
+
+}
